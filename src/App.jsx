6a4c66ad3c644f57d5ffea32b32d86931dca9ffa
Artem <yakovenko.art1@gmail.com>
2024-06-12 23:32:49 +0500

    add redirect for gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import List from "./pages/List";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
 
 export default function App() {
     return (
-        <HashRouter>
-            <section className="app">
+        <section className="app">
+            <BrowserRouter basename="testPizzaSoft">
                 <Routes>
-                    <Route path="/" element={<List />}></Route>
+                    <Route path="/" index element={<List />}></Route>
                     <Route path="/profile" element={<Profile />}></Route>
                     <Route path="*" element={<NotFound />}></Route>
                 </Routes>
-            </section>
-        </HashRouter>
+            </BrowserRouter>
+        </section>
     );
 }
