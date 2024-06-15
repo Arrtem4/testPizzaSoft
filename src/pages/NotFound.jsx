49908579111ee79b972f762b4ca41f2b92d2cdc6
Artem <yakovenko.art1@gmail.com>
2024-06-15 15:20:31 +0500

    add not-found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,15 @@
 export default function NotFound() {
-    return <div>NotFound</div>;
+    return (
+        <section className="not-found center">
+            <section className="not-found-message center">
+                <h1 className="not-found-message-title">Page not found</h1>
+                <button
+                    className="button button__not-found"
+                    onClick={() => window.history.back()}
+                >
+                    Go back
+                </button>
+            </section>
+        </section>
+    );
 }
