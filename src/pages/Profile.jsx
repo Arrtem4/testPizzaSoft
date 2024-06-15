61a27dd03e97e0294fcf439073282e0d94f596d8
Artem <yakovenko.art1@gmail.com>
2024-06-15 23:12:02 +0500

    aft req

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,40 @@
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { BsPersonCircle } from "react-icons/bs";
+import { useState } from "react";
 
 export default function Profile() {
     const { id } = useParams();
 
-    const profile = useSelector((state) => state.dataSlice.profilesList).find(
-        (el) => el.id === +id
-    );
+    const [name, setName] = useState(profile?.name || "");
+    const [birthday, setBirthday] = useState(profile?.birthday || "");
+    const [phone, setPhone] = useState(profile?.phone || "");
+    const [isArchive, setIsArchive] = useState(profile?.isArchive || false);
+    const [role, setRole] = useState(profile?.role || "all");
+
+    
 
     return (
-        <section className="profile-wrapper">
-            <section className="profile">
-                <BsPersonCircle className="profile-card-photo" />
+        <section className="profile-page-wrapper">
+            <section className="profile-page">
+                <section className="profile-page-photo">
+                    <BsPersonCircle className="profile-card-photo" />
+                    <button disabled className="button button__profile_photo">
+                        Upload
+                    </button>
+                </section>
+                <section className="profile-page_settings-element profile-page-name">
+                    <p className="profile-page-titles">Name</p>
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}
+                    />
+                </section>
+                <section className="profile-page_settings-element profile-page-phone"></section>
+                <section className="profile-page_settings-element profile-page-date"></section>
+                <section className="profile-page_settings-element profile-page-position"></section>
+                <section className="profile-page_settings-element profile-page-archive"></section>
             </section>
         </section>
     );
